2f096da3f71df73b993cb51047c7b7181568b455
unknown <kvach2001@gmail.com>
2020-10-27 23:08:51 +0530

    some changes to ui

--- app.js
+++ app.js
@@ -18,32 +18,47 @@ app.get("/", function(req, res) {
 
 app.post("/weather", function(req, res) {
 
-  console.log(req.body);
+  // console.log(req.body);
   const query = req.body.cityName;
   const apiKey = "c2853c7d21168bd2ac2050040edf025e";
   const unit = "metric";
   const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit;
 
   https.get(url, function(response) {
-    console.log(response.statusCode);
+    const statusCode = response.statusCode;
     response.on("data", function(data) {
       const weatherData = JSON.parse(data);
-      const temp = weatherData.main.temp;
-      const feelsLike = weatherData.main.feels_like;
-      const description = weatherData.weather[0].description;
-      // const icon = weatherData.weather[0].icon;
-      // const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+      if(statusCode!=404){
+        console.log(weatherData);
+        const temp = weatherData.main.temp;
+        const feelsLike = weatherData.main.feels_like;
+        const description = weatherData.weather[0].description;
+        const visibility = weatherData.visibility / 1000;
+        const pressure = weatherData.main.pressure;
+        const humidity = weatherData.main.humidity;
 
-      res.render("weather", {
-        weatherDescription: description,
-        query: query,
-        temperature: temp,
-        // imgURL: imageURL
-      });
-      // res.write("<p>The weather is currently " + description + ".</p>");
-      // res.write("<h1>The temperature in "+ query +"is " + temp + " degree Celcius.</h1>");
-      // res.write("<img src=" + imageURL + " >");
-      // res.send();
+        const icon = weatherData.weather[0].icon;
+        const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+
+        res.render("weather", {
+          weatherDescription: description,
+          query: query,
+          temperature: temp,
+          feelsLike: feelsLike,
+          visibility: visibility,
+          pressure: pressure,
+          humidity: humidity,
+          imgURL: imageURL
+        });
+        // res.write("<p>The weather is currently " + description + ".</p>");
+        // res.write("<h1>The temperature in "+ query +"is " + temp + " degree Celcius.</h1>");
+        // res.write("<img src=" + imageURL + " >");
+        // res.send();
+      } else{
+        console.log(statusCode);
+        res.render("pagenotfound");
+      }
+
     });
 
   });
